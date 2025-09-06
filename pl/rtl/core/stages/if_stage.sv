e8a78e986b26d8e9820fa72f1e49bfe82ee576f7
avery <averyw15444@gmail.com>
2025-09-06 14:50:12 -0700

    added instr and regfile memory ip

--- pl/rtl/core/stages/if_stage.sv
+++ pl/rtl/core/stages/if_stage.sv
@@ -15,16 +15,15 @@ pc u_pc(
     .pc       (pc       ),
     .pcplus4  (pcplus4  )
 );
+
 if_id_t if_id_input;
-blk_mem_gen_0 your_instance_name (
-  .clka(clk),    // input wire clka
+
+instr_mem your_instance_name (
+  .clka(clka),    // input wire clka
   .ena(1'b1),      // input wire ena
-  .wea(1'b0),      // input wire [0 : 0] wea
-  .addra(pc),  // input wire [12 : 0] addra
-  .dina(32'b0),    // input wire [31 : 0] dina
+  .addra(pc),  // input wire [11 : 0] addra
   .douta(if_id_input.instr)  // output wire [31 : 0] douta
 );
-
 
 assign if_id_input.pc = pc;
 assign if_id_input.pcplus4 = pcplus4;
