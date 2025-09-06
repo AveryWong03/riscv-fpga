a3c2f9ae445b75854a1f80feee7c2c6e36c18302
avery <averyw15444@gmail.com>
2025-09-06 16:29:34 -0700

    id stage complete

--- pl/rtl/core/regfile.sv
+++ pl/rtl/core/regfile.sv
@@ -8,4 +8,12 @@ module regfile(
     output logic [31:0] rd1, rd2
 );
 
+logic [31:0] rf[31:0];
+
+always_ff @(negedge clk)
+    if(we3) rf[a3] <= wd3;
+
+assign rd1 = (a1 != 0) ? rf[a1] : 0;
+assign rd2 = (a2 != 0) ? rf[a2] : 0;
+
 endmodule
--- pl/rtl/core/stages/id_stage.sv
+++ pl/rtl/core/stages/id_stage.sv
@@ -8,13 +8,51 @@ module id_stage(
     output id_ex_t out // rs1, rs2 need to be output to hazard unit
 );
 
-blk_mem_gen_0 instruction_memory (
-  .clka(clka),    // input wire clka
-  .ena(ena),      // input wire ena
-  .wea(wea),      // input wire [0 : 0] wea
-  .addra(addra),  // input wire [12 : 0] addra
-  .dina(dina),    // input wire [31 : 0] dina
-  .douta(douta)  // output wire [31 : 0] douta
+logic [6:0] op, funct7;
+logic [2:0] funct3;
+logic [4:0] a1, a2;
+logic [1:0] immsrc;
+
+imm_gen u_imm_ged (
+    .instr(in.instr),
+    .immsrc(immsrc),
+    .op(op),
+    .funct3(funct3),
+    .funct7(funct7),
+    .a1(a1),
+    .a2(a2),
+    .rs1(out.rs1),
+    .rs2(out.rs2),
+    .rd(out.rd),
+    .immext(out.immext)
 );
+
+regfile u_regfile (
+    .clk(clk),
+    .a1(a1),
+    .a2(a2),
+    .a3(a3),
+    .wd3(wd3),
+    .we3(we3),
+    .rd1(out.rd1),
+    .rd2(out.rd2)
+);
+
+ctl_unit u_ctl_unit (
+    .op(op),
+    .funct3(funct3),
+    .funct7(funct7),
+    .regwrite(out.regwrite),
+    .memwrite(out.memwrite),
+    .jump(out.jump),
+    .branch(out.branch),
+    .alusrc(out.alusrc),
+    .resultsrc(out.resultsrc),
+    .immsrc(immsrc),
+    .alucontrol(out.alucontrol)
+);
+
+assign out.pc = in.pc;
+assign out.pcplus4 = in.pcplus4;
 
 endmodule
