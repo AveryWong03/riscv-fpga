0cab4bd12e92dc2c304b7a4f4b266189eee8971f
AveryWong03 <averyw15444@gmail.com>
2025-09-07 11:40:15 -0700

    mem stage completed

--- pl/rtl/core/stages/mem_stage.sv
+++ pl/rtl/core/stages/mem_stage.sv
@@ -7,4 +7,20 @@ module mem_stage(
     output mem_wb_t out
 );
 
+regfile_mem u_data_mem (
+  .clka(clk),    // input wire clka
+  .wea(in.memwrite),      // input wire [0 : 0] wea
+  .addra(in.aluresult),  // input wire [4 : 0] addra <= incorrect for now
+  .dina(in.writedata),    // input wire [31 : 0] dina
+  .douta(out.readdata),  // output wire [31 : 0] douta
+);
+
+// Pass through output assignments
+assign out.aluresult = in.aluresult;
+assign out.rd = in.rd;
+assign out.pcplus4 = in.pcplus4;
+assign out.regwrite = in.regwrite;
+assign out.resultsrc = in.resultsrc;
+
+
 endmodule
